f950b8fa6c8e7988215cb75868fb373896ab67b1
Tequila Ridgeway <kelaridgeway@gmail.com>
2024-10-02 13:46:24 -0400

    Strings and Loops

---
+++ strings-and-loops/src/main/java/com/pluralsight/strings/StringsAndLoops.java
@@ -0,0 +1,28 @@
+package com.pluralsight.strings;
+
+public class StringsAndLoops {
+    public static void main(String[] args) {
+        System.out.println("Milk");
+        System.out.println("Eggs");
+        System.out.println("Fruit");
+
+
+
+        String allListItems = "Sugar Bagels Chicken Eggs Apples Gushers";
+
+        String newList = """
+                Sugar
+                    Bagels
+                Chicken
+                    Eggs
+                Apples
+                    Gushers 
+                """;
+        System.out.println(newList);
+
+
+
+    }
+
+}
+
